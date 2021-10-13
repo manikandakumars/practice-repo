4a0e55e829d0e0b76d2d60e4993eb5a20909bc8f
manikandakumars <manikandakuma.s@cognizant.com>
2021-10-12 21:31:02 -0700

    updates

--- netflix-eureka/src/main/java/com/mani/App.java
+++ netflix-eureka/src/main/java/com/mani/App.java
@@ -1,13 +1,18 @@
 package com.mani;
 
-/**
- * Hello world!
- *
- */
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.context.ApplicationContext;
+
+@SpringBootApplication
+@EnableEurekaServer
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "Eureka Discovery" );
+        ApplicationContext context = SpringApplication.run(App.class, args);
+        
     }
 }
